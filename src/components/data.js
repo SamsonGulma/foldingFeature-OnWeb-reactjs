2999a58011264aace2a9ef4a20f0aa402dcb5de9
SamsonGulma <sams0ngultek@gmail.com>
2024-05-11 12:13:31 +0000

    May 11, 2024, 3:13 PM

--- src/components/data.js
+++ src/components/data.js
@@ -7,25 +7,21 @@ const data = [
 
      {
         id: '2',
-        questions: 'what are accordion or foldable components?',
-        answers: 'An accordion or foldable components are a vertically collapsing component that allows users to view one section  of content at a time',
+        questions: 'what are accordion or foldable components used for?',
+        answers: 'to generate vertically stacked content in a page.',
     },
      
       {
         id: '3',
-        questions: 'what are accordion or foldable components?',
-        answers: 'An accordion or foldable components are a vertically collapsing component that allows users to view one section  of content at a time',
+        questions: 'what is the difference between accordion and dropdown',
+        answers: 'The accordion or foldable components will pushdown the content, while the dropdown lays over the content.',
     },
       
        {
         id: '4',
-        questions: 'what are accordion or foldable components?',
-        answers: 'An accordion or foldable components are a vertically collapsing component that allows users to view one section  of content at a time',
-    },
-       
-       {
-        id: '1',
-        questions: 'what are accordion or foldable components?',
-        answers: 'An accordion or foldable components are a vertically collapsing component that allows users to view one section  of content at a time',
+        questions: 'The main benefit of accordion or foldable components',
+        answers: 'allows for the creatin of a collection of components, composed as panels, and arranged in an accordion on a page',
     }
 ];
+
+export default data;
