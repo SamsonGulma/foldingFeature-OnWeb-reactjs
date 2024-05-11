989612c235912462bfcf3c343ee6717ae8f09f7d
SamsonGulma <sams0ngultek@gmail.com>
2024-05-11 13:29:27 +0000

    May 11, 2024, 4:29 PM

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,31 @@
+//*Single selection
+
+import { useState } from "react"
+import { data } from "./data"
+
+//* Multiple selection
+
+
 export default function Foldable() {
-    return <div> Foldable Component</div>
+
+    const [selected, setSelected] = useState(null);
+
+    return  <div className="wrapper">
+        <div className="foldable">
+            {
+                data && data.length > 0 ?(
+                    data.map((dataItem) => (
+                        <div className="item">
+                          <div className="title">
+                            <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            </div>
+                    ))
+                ) : (
+                        <div> No Data Found </div>
+                )
+            }
+        </div>
+        </div>
 }
