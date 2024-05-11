beb0e4c81f232f0f3f48aa3de793b3036bb0327a
SamsonGulma <sams0ngultek@gmail.com>
2024-05-11 12:00:50 +0000

    May 11, 2024, 3:00 PM

---
+++ src/components/data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        id: '1',
+        questions: 'what are accordion or foldable components?',
+        answers: 'An accordion or foldable components are a vertically collapsing component that allows users to view one section  of content at a time',
+    },
+
+     {
+        id: '2',
+        questions: 'what are accordion or foldable components?',
+        answers: 'An accordion or foldable components are a vertically collapsing component that allows users to view one section  of content at a time',
+    },
+     
+      {
+        id: '3',
+        questions: 'what are accordion or foldable components?',
+        answers: 'An accordion or foldable components are a vertically collapsing component that allows users to view one section  of content at a time',
+    },
+      
+       {
+        id: '4',
+        questions: 'what are accordion or foldable components?',
+        answers: 'An accordion or foldable components are a vertically collapsing component that allows users to view one section  of content at a time',
+    },
+       
+       {
+        id: '1',
+        questions: 'what are accordion or foldable components?',
+        answers: 'An accordion or foldable components are a vertically collapsing component that allows users to view one section  of content at a time',
+    }
+];
